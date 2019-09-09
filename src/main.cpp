c05d280decb28194b1c12516477274ff1c7e5f3a
horitahiroshi <horitahiroshi@gmail.com>
2019-09-09 20:20:54 -0300

    Initial commit: first sketch of bluetooth communication for wifi configuration

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,56 @@
+
 #include <Arduino.h>
+#include "BluetoothSerial.h"
+#include "WiFi.h"
+
+BluetoothSerial SerialBT;
+
 
 void setup() {
-  Serial.begin(9600);
+  Serial.begin(115200);
+  SerialBT.begin("ESP32BT");
+
+}
+
+bool WiFi_connected = false;
+
+void setWiFi(){
+  String ssid = "";
+  String password = "";
+  Serial.println("Let's set the WiFi connection!");
+  while(ssid == ""){
+    Serial.println("Please enter WiFi name:");
+    if(SerialBT.available()){
+      ssid = SerialBT.readStringUntil('\n');
+    }
+  }
+  while(password == ""){
+    Serial.println("Please enter WiFi password:");
+    if(SerialBT.available()){
+      password = SerialBT.readStringUntil('\n');
+    }
+  }
+
+  Serial.println(ssid);
+  Serial.println(password);
+
+  WiFi.begin(ssid, password);
+
+  int counter = 6;
+  while (WiFi.status() != WL_CONNECTED || counter >= 0){
+    delay(500);
+    Serial.println("Connecting to WiFi ...");
+    counter--;
+  }
+
+  Serial.println("Connected to the WiFi network.");
 }
 
 void loop() {
-  Serial.println("Hello world!");
+  if(!WiFi_connected){
+    setWiFi();
+    delay(20);
+  }
+  Serial.println("WiFi connected for sure!");
   delay(1000);
 }
---
+++ test/README.md
@@ -0,0 +1,26 @@
+# ESP32 WiFi project
+
+## Testing BT comm between Ubuntu and ESP32
+
+For testing BT communication between Ubuntu 10.04 and ESP32, follow steps:
+
+1. Find the ESP32 Bluetooth identification
+```
+hcitool scan
+```
+It must return something like this:
+```
+3C:71:BF:42:34:16   ESP32BT
+```
+2. Connect the ESP32BT
+```
+sudo rfcomm connect /dev/rfcomm0 3C:71:BF:42:34:16 1 &
+```
+If necessary, clean up all BT connections before with the command:
+```
+sudo killall rfcomm
+```
+3. Initiate the Serial BT communication
+```
+sudo screen /dev/rfcomm0
+```
