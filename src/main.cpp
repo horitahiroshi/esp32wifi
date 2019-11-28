64fddcc745e63c5a8e950771c1737aa3e7171054
horitahiroshi <horitahiroshi@gmail.com>
2019-11-28 14:10:06 -0300

    backup commit

--- src/main.cpp
+++ src/main.cpp
@@ -1,63 +1,151 @@
+/*********
+  Rui Santos
+  Complete project details at http://randomnerdtutorials.com  
+*********/
 
-#include <Arduino.h>
-#include "BluetoothSerial.h"
+// Load Wi-Fi library
 #include "WiFi.h"
+#include "setupWifi.h"
 
-BluetoothSerial SerialBT;
+SmartConfig Smartconfig;
 
+// Replace with your network credentials
+// const char* ssid     = "LRM";
+// const char* password = "m8h0d@g!";
+
+// Set web server port number to 80
+WiFiServer server(80);
+
+// Variable to store the HTTP request
+String header;
+
+// Auxiliar variables to store the current output state
+String output26State = "off";
+String output27State = "off";
+
+// Assign output variables to GPIO pins
+const int output26 = 26;
+const int output27 = 27;
 
 void setup() {
   Serial.begin(115200);
-  SerialBT.begin("ESP32BT");
+  // Initialize the output variables as outputs
+  pinMode(output26, OUTPUT);
+  pinMode(output27, OUTPUT);
+  // Set outputs to LOW
+  digitalWrite(output26, LOW);
+  digitalWrite(output27, LOW);
 
+  Smartconfig.setupWifi();
+
+  // // Connect to Wi-Fi network with SSID and password
+  // Serial.print("Connecting to ");
+  // Serial.println(ssid);
+  // WiFi.begin(ssid, password);
+  // while (WiFi.status() != WL_CONNECTED) {
+  //   delay(500);
+  //   Serial.print(".");
+  // }
+  // // Print local IP address and start web server
+  // Serial.println("");
+  // Serial.println("WiFi connected.");
+  // Serial.println("IP address: ");
+  // Serial.println(WiFi.localIP());
+  
+  server.begin();
 }
 
-bool WiFi_connected = false;
+void loop(){
+  WiFiClient client = server.available();   // Listen for incoming clients
 
-void setWiFi(){
-  String ssid = "";
-  String password = "";
-  Serial.println("Let's set the WiFi connection!");
-  Serial.println("Please enter WiFi name:");
-  while(ssid == ""){
-    ssid = SerialBT.readStringUntil('\n');
-  }
-  Serial.println(ssid);
-  Serial.println(ssid.length());
-
-  Serial.println("Please enter WiFi password:");
-  while(password == ""){
-    password = SerialBT.readStringUntil('\n');
-  }
-  Serial.println(password);
-  Serial.println(password.length());
-
-  WiFi.begin(ssid.substring(0,ssid.length()-1).c_str(), password.substring(0,password.length()-1).c_str());
-
-  int counter = 20;
-  while (WiFi.status() != WL_CONNECTED){
-    delay(1000);
-    Serial.println("Connecting to WiFi ...");
-    Serial.println(counter);
-    counter--;
-    if(counter <= 0){
-      Serial.println("Timed out!");
-      break;
+  if (client) {                             // If a new client connects,
+    Serial.println("New Client.");          // print a message out in the serial port
+    String currentLine = "";                // make a String to hold incoming data from the client
+    while (client.connected()) {            // loop while the client's connected
+      if (client.available()) {             // if there's bytes to read from the client,
+        char c = client.read();             // read a byte, then
+        Serial.write(c);                    // print it out the serial monitor
+        header += c;
+        if (c == '\n') {                    // if the byte is a newline character
+          // if the current line is blank, you got two newline characters in a row.
+          // that's the end of the client HTTP request, so send a response:
+          if (currentLine.length() == 0) {
+            // HTTP headers always start with a response code (e.g. HTTP/1.1 200 OK)
+            // and a content-type so the client knows what's coming, then a blank line:
+            client.println("HTTP/1.1 200 OK");
+            client.println("Content-type:text/html");
+            client.println("Connection: close");
+            client.println();
+            
+            // turns the GPIOs on and off
+            if (header.indexOf("GET /26/on") >= 0) {
+              Serial.println("GPIO 26 on");
+              output26State = "on";
+              digitalWrite(output26, HIGH);
+            } else if (header.indexOf("GET /26/off") >= 0) {
+              Serial.println("GPIO 26 off");
+              output26State = "off";
+              digitalWrite(output26, LOW);
+            } else if (header.indexOf("GET /27/on") >= 0) {
+              Serial.println("GPIO 27 on");
+              output27State = "on";
+              digitalWrite(output27, HIGH);
+            } else if (header.indexOf("GET /27/off") >= 0) {
+              Serial.println("GPIO 27 off");
+              output27State = "off";
+              digitalWrite(output27, LOW);
+            }
+            
+            // Display the HTML web page
+            client.println("<!DOCTYPE html><html>");
+            client.println("<head><meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">");
+            client.println("<link rel=\"icon\" href=\"data:,\">");
+            // CSS to style the on/off buttons 
+            // Feel free to change the background-color and font-size attributes to fit your preferences
+            client.println("<style>html { font-family: Helvetica; display: inline-block; margin: 0px auto; text-align: center;}");
+            client.println(".button { background-color: #4CAF50; border: none; color: white; padding: 16px 40px;");
+            client.println("text-decoration: none; font-size: 30px; margin: 2px; cursor: pointer;}");
+            client.println(".button2 {background-color: #555555;}</style></head>");
+            
+            // Web Page Heading
+            client.println("<body><h1>ESP32 Web Server</h1>");
+            
+            // Display current state, and ON/OFF buttons for GPIO 26  
+            client.println("<p>GPIO 26 - State " + output26State + "</p>");
+            // If the output26State is off, it displays the ON button       
+            if (output26State=="off") {
+              client.println("<p><a href=\"/26/on\"><button class=\"button\">ON</button></a></p>");
+            } else {
+              client.println("<p><a href=\"/26/off\"><button class=\"button button2\">OFF</button></a></p>");
+            } 
+               
+            // Display current state, and ON/OFF buttons for GPIO 27  
+            client.println("<p>GPIO 27 - State " + output27State + "</p>");
+            // If the output27State is off, it displays the ON button       
+            if (output27State=="off") {
+              client.println("<p><a href=\"/27/on\"><button class=\"button\">ON</button></a></p>");
+            } else {
+              client.println("<p><a href=\"/27/off\"><button class=\"button button2\">OFF</button></a></p>");
+            }
+            client.println("</body></html>");
+            
+            // The HTTP response ends with another blank line
+            client.println();
+            // Break out of the while loop
+            break;
+          } else { // if you got a newline, then clear currentLine
+            currentLine = "";
+          }
+        } else if (c != '\r') {  // if you got anything else but a carriage return character,
+          currentLine += c;      // add it to the end of the currentLine
+        }
+      }
     }
+    // Clear the header variable
+    header = "";
+    // Close the connection
+    client.stop();
+    Serial.println("Client disconnected.");
+    Serial.println("");
   }
-  if(counter > 0){
-    Serial.println("Connected to the WiFi network.");
-    WiFi_connected = true;
-  }
-}
-
-void loop() {
-  if(!WiFi_connected){
-    setWiFi();
-    delay(20);
-  } 
-  else {
-    Serial.println("WiFi connected for sure!");
-  }
-  delay(1000);
 }
---
+++ src/setupWifi.h
@@ -0,0 +1,15 @@
+#ifndef setupwifi_h
+#define setupwifi_h
+
+#include "WiFi.h"
+
+class SmartConfig
+{
+  public:
+    // SmartConfig();
+    void setupWifi();
+  private:
+    
+};
+
+#endif
